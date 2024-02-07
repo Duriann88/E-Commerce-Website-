861807b5208ac20e56d0e7d6e49ed9cecd6fc7cd
Duriann88 <S10258761@connect.np.edu.sg>
2024-02-07 22:18:35 +0800

    bdgd

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,1 +1,59 @@
 
+"use client";
+import { useState } from "react";
+
+const NAMES = [
+  "Andrea",
+  "Billie",
+  "Clara",
+  "David",
+  "Eva",
+  "Frank",
+  "Grace",
+  "Henry",
+  "Ivy",
+  "Jack",
+  "Katherine",
+  "Leo",
+  "Mia",
+  "Nathan",
+  "Olivia",
+  "Peter",
+  "Quinn",
+  "Rachel",
+  "Samuel",
+  "Taylor",
+  "Ursula",
+  "Victor",
+  "Wendy",
+  "Xander",
+  "Yvonne",
+  "Zachary",
+];
+
+export default function App() {
+  const [filteredNames, setFilteredNames] = useState(NAMES);
+  let timeoutId;
+
+  function handleKeyUp(event) {
+    const query = event.target.value.toLowerCase();
+
+    // Clear the previous timeout
+    clearTimeout(timeoutId);
+
+    // Set a new timeout
+    timeoutId = setTimeout(() => {
+      setFilteredNames(NAMES.filter((name) => name.toLowerCase().includes(query)));
+    }, 200);
+  }
+  return (
+    <div>
+      <input onKeyUp={handleKeyUp} />
+      <ul>
+        {filteredNames.map((name) => {
+          return <li key={name}>{name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
