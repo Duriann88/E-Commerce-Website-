63365c40979cb6aa999a0863983f4aa0ff15c88f
Duriann88 <S10258761@connect.np.edu.sg>
2024-02-07 22:22:39 +0800

    dgf

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,59 +1,14 @@
+// pages/Profile.js
 
-"use client";
-import { useState } from "react";
+import React from 'react';
 
-const NAMES = [
-  "Andrea",
-  "Billie",
-  "Clara",
-  "David",
-  "Eva",
-  "Frank",
-  "Grace",
-  "Henry",
-  "Ivy",
-  "Jack",
-  "Katherine",
-  "Leo",
-  "Mia",
-  "Nathan",
-  "Olivia",
-  "Peter",
-  "Quinn",
-  "Rachel",
-  "Samuel",
-  "Taylor",
-  "Ursula",
-  "Victor",
-  "Wendy",
-  "Xander",
-  "Yvonne",
-  "Zachary",
-];
-
-export default function App() {
-  const [filteredNames, setFilteredNames] = useState(NAMES);
-  let timeoutId;
-
-  function handleKeyUp(event) {
-    const query = event.target.value.toLowerCase();
-
-    // Clear the previous timeout
-    clearTimeout(timeoutId);
-
-    // Set a new timeout
-    timeoutId = setTimeout(() => {
-      setFilteredNames(NAMES.filter((name) => name.toLowerCase().includes(query)));
-    }, 200);
-  }
+const Profile = () => {
   return (
     <div>
-      <input onKeyUp={handleKeyUp} />
-      <ul>
-        {filteredNames.map((name) => {
-          return <li key={name}>{name}</li>;
-        })}
-      </ul>
+      <h1>Profile Page</h1>
+      {/* Add profile information here */}
     </div>
   );
-}
+};
+
+export default Profile;
