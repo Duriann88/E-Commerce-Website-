5e5ce06bc28a55fc64fd26cbc67412f65f5a458f
jadenljb <150576943+jadenljb@users.noreply.github.com>
2024-02-08 17:08:10 +0800

    Update page.jsx

--- app/cancel/page.jsx
+++ app/cancel/page.jsx
@@ -1,6 +1,17 @@
+import { Button } from "react-bootstrap";
+
 function Cancel(){
     return (
-        <h1>Sorry to see that you cancelled your Stripe payment.</h1>
+        <>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <h1 align='center' className='g-3'>Successful payment has been made. Thank you!</h1>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+            <Button variant='dark'>
+                <a href='/products'>Continue Shopping</a>
+            </Button>
+            </div>
+        </>
     )
 }
 
