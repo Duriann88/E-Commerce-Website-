454e2ba8869a0bf5d3c1cddd479c6a5c131ae706
jadenljb <150576943+jadenljb@users.noreply.github.com>
2024-02-08 10:59:27 +0800

    Update productsStore.js

--- app/productsStore.js
+++ app/productsStore.js
@@ -1,3 +1,5 @@
+// Creating an array will allow easier access to the product and its details that can be used for other pages.
+// Array shows: the id, the title, the price and the image.
 const productsArray = [
     {
         id: "price_1Oh5HnBq8eoYGYKSKuPtx7zF",
@@ -20,15 +22,16 @@ const productsArray = [
     }
 ]
 
-function getProductData(id){
-    let productData = productsArray.find(product => product.id === id);
 
-    if (productData == undefined){
+function getProductData(id){
+    let productData = productsArray.find(product => product.id === id); // Searches for the product id
+
+    if (productData == undefined){ // Checks if the product actually exists.
         console.log("Product data does not exist for ID: " + id)
         return undefined;
     }
     
-    return productData;
+    return productData; // Returns the data for use in other pages.
 }
 
 export { productsArray, getProductData };
