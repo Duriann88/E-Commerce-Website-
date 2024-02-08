2eae33ff59dda91f8e6ceccd293627d3550fcf49
Duriann88 <S10258761@connect.np.edu.sg>
2024-02-08 23:15:13 +0800

    hh

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,18 @@
 import Navbar from "@/app/navbar/navbar";
 import './globals.css';
 
+// Define the RootLayout component
 export default function RootLayout({ children }) {
   return (
+    // Root HTML structure for the layout
     <html lang="en">
       <body>
+        {/* Include the Navbar component */}
         <Navbar />
+        {/* Render the children components */}
         {children}
       </body>
     </html>
   );
 }
+
