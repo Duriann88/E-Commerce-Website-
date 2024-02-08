c349300b8420560b435f251a6042fe16729cdfc9
Duriann88 <S10258761@connect.np.edu.sg>
2024-02-08 09:59:00 +0800

    dc

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,4 +1,4 @@
-import React from 'react' ;
+import React from 'react';
 import { FaUser, FaMapMarkerAlt, FaRegCalendar, FaShoppingBag, FaLock, FaUserShield } from 'react-icons/fa';
 
 const Profile = () => {
