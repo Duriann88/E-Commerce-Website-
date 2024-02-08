f8acfa8adeb181c91c13e1913c0f4c514b31fe97
jadenljb <150576943+jadenljb@users.noreply.github.com>
2024-02-08 20:59:06 +0800

    Update page.jsx

--- app/cancel/page.jsx
+++ app/cancel/page.jsx
@@ -1,14 +1,18 @@
+// Jaden - Full Ownership
+
 import { Button } from "react-bootstrap";
 
+// This function is the cancel page if the user decides to cancel the transaction.
+// It prints a cancel message and has a button for the user to click if they choose to continue browsing the products.
 function Cancel(){
     return (
         <>
             <div style={{display: 'flex', justifyContent: 'center'}}>
-                <h1 align='center' className='g-3'>Successful payment has been made. Thank you!</h1>
+                <h1 align='center' className='g-3'>Transaction cancelled.</h1> 
             </div>
             <div style={{display: 'flex', justifyContent: 'center'}}>
             <Button variant='dark'>
-                <a href='/products'>Continue Shopping</a>
+                <a href='/products'>Continue Shopping</a> 
             </Button>
             </div>
         </>
