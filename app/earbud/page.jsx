2bbde34761e6d1ada96ce589c6da5e1e37165cbe
Duriann88 <S10258761@connect.np.edu.sg>
2024-02-08 16:31:31 +0800

    m

--- app/earbud/page.jsx
+++ app/earbud/page.jsx
@@ -2,7 +2,7 @@
 //Student No.: S10258236F
 "use client";
 import React, { useState } from 'react';
-import CartModal from '@/components/cartModal';
+import CartModal from '@/components/CartModal';
 import { Button } from 'react-bootstrap';
 import Link from 'next/link';
 
