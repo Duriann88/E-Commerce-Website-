90da687136674c779432809a21adf1a3dca52030
jadenljb <150576943+jadenljb@users.noreply.github.com>
2024-02-08 23:41:45 +0800

    Update page.jsx

--- app/success/page.jsx
+++ app/success/page.jsx
@@ -9,7 +9,7 @@ function Success(){
     return (
         <>
             <div style={{display: 'flex', justifyContent: 'center'}}>
-                <h1 align='center' className='g-3'>Sorry to see that you cancelled your Stripe payment.</h1>
+                <h1 align='center' className='g-3'>Transaction completed. Thank you for shopping with us!</h1>
             </div>
             <div style={{display: 'flex', justifyContent: 'center'}}>
             <Button variant='dark'>
