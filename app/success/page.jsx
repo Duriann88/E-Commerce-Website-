2d60518c9527676a8bfbab60c76409d50fc81c89
jadenljb <150576943+jadenljb@users.noreply.github.com>
2024-02-08 17:07:39 +0800

    Update page.jsx

--- app/success/page.jsx
+++ app/success/page.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 function Success(){
     return (
-        <h1>Success payment made. Thank you for shopping with us!</h1>
+        <>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <h1 align='center' className='g-3'>Sorry to see that you cancelled your Stripe payment.</h1>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+            <Button variant='dark'>
+                <a href='/products'>Continue Shopping</a>
+            </Button>
+            </div>
+        </>
     )
 }
 
